2de7c2efb71bdfba7cc4ead2ec121ddf2af9d7df
Kazz2433 <kelvinquida24@gmail.com>
2023-09-11 08:56:56 -0400

    chore: style heading

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import styled from "styled-components";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import Button from './ui/Button'
 import Input from './ui/Input'
+import Heading from './ui/Heading'
 
 export default function App() {
 
-  const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-    background-color: yellow;
-  `
  
  const StyledApp = styled.div`
  background-color: orangered;
@@ -19,10 +15,14 @@ export default function App() {
   return (
     <StyledApp>
       <GlobalStyles />
-      <H1>askdhsajkldhjl</H1>
+      <Heading as="h1">The Wild Oasis</Heading>
+
+      <Heading as="h2">Check in and out</Heading>
       <Button>Check in</Button>
       <Button>Check out</Button>
 
+      <Heading as="h3">Form</Heading>
+      <Input type="number" placeholder="Number of guests" />
       <Input type="number" placeholder="Number of guests" />
     </StyledApp>
   )
