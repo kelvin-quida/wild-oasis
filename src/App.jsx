a13999654c47a2b9d0759e84242e94105223ef6e
Kazz2433 <kelvinquida24@gmail.com>
2023-09-11 09:40:11 -0400

    chore: setting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,34 @@
-import styled from "styled-components";
-import { GlobalStyles } from "./styles/GlobalStyles";
-import Button from './ui/Button'
-import Input from './ui/Input'
-import Heading from './ui/Heading'
-import Row from "./ui/Row";
+import { RouterProvider } from 'react-router-dom'
+import {createBrowserRouter} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
+import { Navigate } from 'react-router-dom'
+import {GlobalStyles} from './styles/GlobalStyles'
+
+const router = createBrowserRouter([
+  {path:'/',element:<Navigate to='/dashboard' replace/>},
+  {path:'/dashboard',element:<Dashboard/>},
+  {path:'/bookings',element:<Bookings/>},
+  {path:'/cabins',element:<Cabins/>},
+  {path:'/users',element:<Users/>},
+  {path:'/settings',element:<Settings/>},
+  {path:'/account',element:<Account/>},
+  {path:'/login',element:<Login/>},
+  {path:'/*',element:<PageNotFound/>},
+])
 
 export default function App() {
-
- 
- const StyledApp = styled.div`
- padding: 20px;
- `
-
   return (
-    <StyledApp>
+    <>
       <GlobalStyles />
-      <Row >
-        <Row type='horizontal'>
-          <Heading as="h1">The Wild Oasis</Heading>
-          <div>
-
-            <Heading as="h2">Check in and out</Heading>
-            <Button >Check in</Button>
-            <Button variation='secondary' size='small'>Check out</Button>
-
-          </div>
-        </Row>
-
-        <Row>
-          <Heading as="h3">Form</Heading>
-          <form>
-            <Input type="number" placeholder="Number of guests" />
-            <Input type="number" placeholder="Number of guests" />
-          </form>
-        </Row>
-      </Row>
-    </StyledApp>
+        <RouterProvider router={router} />
+    </>
   )
 }
+
