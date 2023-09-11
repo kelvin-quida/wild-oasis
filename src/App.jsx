0adf75a651e4e8c5f0a64511023f100a23a3e821
Kazz2433 <kelvinquida24@gmail.com>
2023-09-11 09:16:41 -0400

    chore: style Button

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,38 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 import Button from './ui/Button'
 import Input from './ui/Input'
 import Heading from './ui/Heading'
+import Row from "./ui/Row";
 
 export default function App() {
 
  
  const StyledApp = styled.div`
- background-color: orangered;
  padding: 20px;
  `
 
   return (
     <StyledApp>
       <GlobalStyles />
-      <Heading as="h1">The Wild Oasis</Heading>
+      <Row >
+        <Row type='horizontal'>
+          <Heading as="h1">The Wild Oasis</Heading>
+          <div>
 
-      <Heading as="h2">Check in and out</Heading>
-      <Button>Check in</Button>
-      <Button>Check out</Button>
+            <Heading as="h2">Check in and out</Heading>
+            <Button >Check in</Button>
+            <Button variation='secondary' size='small'>Check out</Button>
 
-      <Heading as="h3">Form</Heading>
-      <Input type="number" placeholder="Number of guests" />
-      <Input type="number" placeholder="Number of guests" />
+          </div>
+        </Row>
+
+        <Row>
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </Row>
     </StyledApp>
   )
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -51,21 +51,18 @@ const variations = {
 };
 
 const Button = styled.button`
-font-size: 1.4rem;
-padding: 1.2rem 1.6rem;
-font-weight: 500;
-border: none;
-border-radius: var(--border-radius-sm);
-background-color: var(--color-brand-600);
-color: var(--color-brand-50);
-box-shadow: var(--shadow-sm);
-cursor: pointer;
-
-
-&:hover{
-  background-color: var(--color-brand-700);
-}
+  border: none;
+  border-radius: var(--border-radius-sm); 
+  box-shadow: var(--shadow-sm);
+ 
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
+  
 `
+Button.defaultProps ={
+  variation:'primary',
+  size:'medium'
+}
 
 
 export default Button
