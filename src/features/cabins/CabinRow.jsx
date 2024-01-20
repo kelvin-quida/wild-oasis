081d8a9d6ef7dad4709542728ea8a8cd2f03f402
Kazz2433 <kelvinquida24@gmail.com>
2024-01-20 15:38:57 -0400

    deleting cabin rows

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import { formatCurrency } from "../../utils/helpers";
-import { useState } from "react";
+import { formatCurrency } from "../../utils/helpers"; 
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 const TableRow = styled.div`
   display: grid;
@@ -45,8 +46,7 @@ const Discount = styled.div`
   color: var(--color-green-700);
 `;
 
-export default function CabinRow({cabin}){
-  const [showForm,setShowForm] = useState(false)
+export default function CabinRow({cabin}){ 
 
   const { isDeleting, deleteCabin} = useDeleteCabin()
 
@@ -84,12 +84,24 @@ export default function CabinRow({cabin}){
         {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
         <div>
           <button onClick={handleDuplicate} disabled={isCreating}><HiSquare2Stack/></button>
-          <button onClick={()=> setShowForm((show) => !show)}>            <HiPencil /></button>
-
-          <button onClick={()=> deleteCabin(cabinId)} disabled={isDeleting}> <HiTrash /></button>
+          <Modal>
+            <Modal.Open opens='edit'>
+              <button> <HiPencil /></button>
+            </Modal.Open>
+            <Modal.Window name='edit'>
+              <CreateCabinForm cabinToEdit={cabin}/>
+            </Modal.Window>
+ 
+            <Modal.Open>
+              <button> <HiTrash /></button>
+            </Modal.Open>
+            <Modal.Window>
+              <ConfirmDelete resourceName='cabins' onConfirm={()=> deleteCabin(cabinId)} />
+            </Modal.Window>
+ 
+          </Modal>
         </div>
       </TableRow>
-      {showForm && <CreateCabinForm cabinToEdit={cabin}/>}
     </>
   )
 }
---
+++ src/hooks/useClickOutside.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react"
+
+
+export default function useClickOutside(handler){
+    const ref = useRef()
+    useEffect(()=>{
+        const handleClick = (e) => {
+          if(ref.current && !ref.current.contains(e.target)){
+            handler()
+          }
+        }
+      
+        document.addEventListener('click',handleClick, true)
+      
+        return () => document.removeEventListener('click',handleClick, true)
+      },[handler])
+
+    return ref
+}
+
