61cb011aba0a7df55a42e44949480c0563557005
Kazz2433 <kelvinquida24@gmail.com>
2024-02-17 19:21:25 -0400

    feat: check out

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -7,8 +7,9 @@ import Table from "../../ui/Table";
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import Menus from "../../ui/Menus";
-import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import useCheckout from "../check-in-out/useCheckout";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -52,11 +53,13 @@ export default function BookingRow({
   },
 }) {
   const navigate = useNavigate()
+  const {checkout,isCheckingOut} = useCheckout()
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
     "checked-out": "silver",
   };
+
 
   return (
     <Table.Row>
@@ -89,6 +92,7 @@ export default function BookingRow({
         <Menus.List id={bookingId} >
             <Menus.Button icon={<HiEye />} onClick={() => navigate(`/bookings/${bookingId}`)} > See Details </Menus.Button>
             {status === 'unconfirmed' && (<Menus.Button icon={<HiArrowDownOnSquare />} onClick={() => navigate(`/checkin/${bookingId}`)} > See Details </Menus.Button>)}
+            {status === 'checked-in' && (<Menus.Button icon={<HiArrowUpOnSquare />} onClick={() => checkout(bookingId)} disabled={isCheckingOut} > Check Out </Menus.Button>)}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
