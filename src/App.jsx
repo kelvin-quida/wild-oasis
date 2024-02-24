e5322b6a9a295193fceb31dc18b4c3ea4cd61d02
Kazz2433 <kelvinquida24@gmail.com>
2024-02-24 16:04:43 -0400

    login with auth

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'react-hot-toast'
 import Booking from './pages/Booking'
 import Checkin from './pages/Checkin'
+import ProtectedRoute from './ui/ProtectedRoute'
 
 
 
@@ -28,17 +29,21 @@ const queryClient = new QueryClient({
 })
 
 const router = createBrowserRouter([
-  {element:<AppLayout />,children:[
-    {path:'/',element:<Navigate to='/dashboard' replace/>},
-    {path:'/dashboard',element:<Dashboard/>},
-    {path:'/bookings',element:<Bookings/>},
-    {path:'/bookings/:bookingId',element:<Booking/>},
-    {path:'/checkin/:bookingId',element:<Checkin/>},
-    {path:'/cabins',element:<Cabins/>},
-    {path:'/users',element:<Users/>},
-    {path:'/settings',element:<Settings/>},
-    {path:'/account',element:<Account/>},
-  ]},
+    {element:
+    <ProtectedRoute>
+      <AppLayout />
+    </ProtectedRoute>
+    ,children:[
+      {path:'/',element:<Navigate to='/dashboard' replace/>},
+      {path:'/dashboard',element:<Dashboard/>},
+      {path:'/bookings',element:<Bookings/>},
+      {path:'/bookings/:bookingId',element:<Booking/>},
+      {path:'/checkin/:bookingId',element:<Checkin/>},
+      {path:'/cabins',element:<Cabins/>},
+      {path:'/users',element:<Users/>},
+      {path:'/settings',element:<Settings/>},
+      {path:'/account',element:<Account/>},
+    ]},
   {path:'/login',element:<Login/>},
   {path:'/*',element:<PageNotFound/>},
 ])
